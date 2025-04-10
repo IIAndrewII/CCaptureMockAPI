a94b9a7821f6a17fbfbbeeb4a4e7c4a07abfab5e
Andrew <andrew.samy.hanna1998@gmail.com>
2025-04-10 15:34:39 +0200

    Add WinForm

---
+++ CCaptureWinForm/MainForm.cs
@@ -0,0 +1,20 @@
+namespace CCaptureWinForm
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txtPassword_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
