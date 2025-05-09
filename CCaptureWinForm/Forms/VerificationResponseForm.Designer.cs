ec1f3aaf5852c0fb778b5642358aa93d528e111a
Andrew <andrew.samy.hanna1998@gmail.com>
2025-05-09 14:24:02 +0300

    VerificationResponseForm V2

--- CCaptureWinForm/Forms/VerificationResponseForm.Designer.cs
+++ CCaptureWinForm/Forms/VerificationResponseForm.Designer.cs
@@ -2,15 +2,8 @@
 {
     partial class VerificationResponseForm
     {
-        /// <summary>
-        /// Required designer variable.
-        /// </summary>
         private System.ComponentModel.IContainer components = null;
 
-        /// <summary>
-        /// Clean up any resources being used.
-        /// </summary>
-        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
         protected override void Dispose(bool disposing)
         {
             if (disposing && (components != null))
@@ -20,60 +13,62 @@
             base.Dispose(disposing);
         }
 
-        #region Windows Form Designer generated code
-
-        /// <summary>
-        /// Required method for Designer support - do not modify
-        /// the contents of this method with the code editor.
-        /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             this.treeView = new System.Windows.Forms.TreeView();
             this.propertyGrid = new System.Windows.Forms.PropertyGrid();
             this.splitContainer = new System.Windows.Forms.SplitContainer();
+            this.documentsGrid = new System.Windows.Forms.DataGridView();
             ((System.ComponentModel.ISupportInitialize)(this.splitContainer)).BeginInit();
             this.splitContainer.Panel1.SuspendLayout();
             this.splitContainer.Panel2.SuspendLayout();
-            this.splitContainer.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.documentsGrid)).BeginInit();
             this.SuspendLayout();
-            // 
+
             // treeView
-            // 
             this.treeView.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.treeView.Location = new System.Drawing.Point(0, 30);
+            this.treeView.Location = new System.Drawing.Point(0, 90);
             this.treeView.Name = "treeView";
-            this.treeView.Size = new System.Drawing.Size(400, 570);
+            this.treeView.Size = new System.Drawing.Size(400, 510);
             this.treeView.TabIndex = 0;
             this.treeView.AfterSelect += new System.Windows.Forms.TreeViewEventHandler(this.treeView_AfterSelect);
-            // 
+            this.treeView.NodeMouseHover += new System.Windows.Forms.TreeNodeMouseHoverEventHandler(this.treeView_NodeMouseHover);
+
+            // ImageList for treeView icons
+            this.treeView.ImageList = new ImageList(this.components);
+            this.treeView.ImageList.Images.Add("Root", System.Drawing.SystemIcons.Information.ToBitmap());
+            this.treeView.ImageList.Images.Add("Batch", System.Drawing.SystemIcons.Application.ToBitmap());
+
             // propertyGrid
-            // 
             this.propertyGrid.Dock = System.Windows.Forms.DockStyle.Fill;
             this.propertyGrid.Location = new System.Drawing.Point(0, 0);
             this.propertyGrid.Name = "propertyGrid";
             this.propertyGrid.Size = new System.Drawing.Size(400, 600);
             this.propertyGrid.TabIndex = 1;
             this.propertyGrid.PropertyValueChanged += new System.Windows.Forms.PropertyValueChangedEventHandler(this.propertyGrid_PropertyValueChanged);
-            // 
+
+            // documentsGrid
+            this.documentsGrid.Dock = System.Windows.Forms.DockStyle.Top;
+            this.documentsGrid.Location = new System.Drawing.Point(0, 0);
+            this.documentsGrid.Name = "documentsGrid";
+            this.documentsGrid.Size = new System.Drawing.Size(400, 200);
+            this.documentsGrid.TabIndex = 2;
+            this.documentsGrid.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
+            this.documentsGrid.ReadOnly = true;
+
             // splitContainer
-            // 
             this.splitContainer.Dock = System.Windows.Forms.DockStyle.Fill;
             this.splitContainer.Location = new System.Drawing.Point(0, 0);
             this.splitContainer.Name = "splitContainer";
-            // 
-            // splitContainer.Panel1
-            // 
             this.splitContainer.Panel1.Controls.Add(this.treeView);
-            // 
-            // splitContainer.Panel2
-            // 
+            this.splitContainer.Panel1.Controls.Add(this.documentsGrid);
             this.splitContainer.Panel2.Controls.Add(this.propertyGrid);
             this.splitContainer.Size = new System.Drawing.Size(800, 600);
             this.splitContainer.SplitterDistance = 400;
-            this.splitContainer.TabIndex = 2;
-            // 
+            this.splitContainer.TabIndex = 3;
+
             // VerificationResponseForm
-            // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 600);
@@ -82,15 +77,15 @@
             this.Text = "Verification Response Viewer";
             this.splitContainer.Panel1.ResumeLayout(false);
             this.splitContainer.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.documentsGrid)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.splitContainer)).EndInit();
             this.splitContainer.ResumeLayout(false);
             this.ResumeLayout(false);
         }
 
-        #endregion
-
         private System.Windows.Forms.TreeView treeView;
         private System.Windows.Forms.PropertyGrid propertyGrid;
         private System.Windows.Forms.SplitContainer splitContainer;
+        private System.Windows.Forms.DataGridView documentsGrid;
     }
 }
--- CCaptureWinForm/Forms/VerificationResponseForm.cs
+++ CCaptureWinForm/Forms/VerificationResponseForm.cs
@@ -1,10 +1,9 @@
 ﻿using System;
-using System.Windows.Forms;
-using System.Text.Json;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
+using System.Windows.Forms;
 using CCaptureWinForm.Core.Entities;
-using Microsoft.IdentityModel.Tokens;
 
 namespace VerificationResponseViewer
 {
@@ -13,15 +12,20 @@ namespace VerificationResponseViewer
         private object _selectedObject;
         private List<VerificationResponse> _verificationResponses;
         private ComboBox comboBoxResponses;
+        private TextBox searchBox;
+        private TextBox filterTextBox;
 
         public VerificationResponseForm(List<VerificationResponse> verificationResponses)
         {
-            _verificationResponses = verificationResponses;
+            _verificationResponses = verificationResponses.OrderByDescending(vr => vr.ExecutionDate).ToList();
             InitializeComponent();
             InitializeComboBox();
-            if (verificationResponses.Any())
+            InitializeFilter();
+            InitializeSearch();
+            if (_verificationResponses.Any())
             {
-                InitializeTreeView(verificationResponses.First());
+                InitializeTreeView(_verificationResponses.First());
+                InitializeDocumentsGrid(_verificationResponses.First());
                 comboBoxResponses.SelectedIndex = 0;
             }
         }
@@ -35,45 +39,101 @@ namespace VerificationResponseViewer
             };
             comboBoxResponses.SelectedIndexChanged += ComboBoxResponses_SelectedIndexChanged;
 
-            for (int i = 0; i < _verificationResponses.Count; i++)
-            {
-                comboBoxResponses.Items.Add($"Verification Response {i + 1} (Batch: {_verificationResponses[i].Batch.Name})");
-            }
+            UpdateComboBoxItems(_verificationResponses);
 
             splitContainer.Panel1.Controls.Add(comboBoxResponses);
-            treeView.Top = comboBoxResponses.Height;
-            treeView.Height -= comboBoxResponses.Height;
+        }
+
+        private void InitializeFilter()
+        {
+            filterTextBox = new TextBox
+            {
+                Dock = DockStyle.Top,
+                PlaceholderText = "Filter by batch name or status"
+            };
+            filterTextBox.TextChanged += (s, e) =>
+            {
+                var filter = filterTextBox.Text.ToLower();
+                var filteredResponses = _verificationResponses
+                    //.Where(vr => vr.Batch.Name.ToLower().Contains(filter) || vr.Status.ToLower().Contains(filter))
+                    .Where(vr => vr.Batch.Name.ToLower().Contains(filter))
+                    .ToList();
+                comboBoxResponses.Items.Clear();
+                UpdateComboBoxItems(filteredResponses);
+                if (filteredResponses.Any())
+                {
+                    comboBoxResponses.SelectedIndex = 0;
+                    InitializeTreeView(filteredResponses.First());
+                    InitializeDocumentsGrid(filteredResponses.First());
+                }
+            };
+            splitContainer.Panel1.Controls.Add(filterTextBox);
+        }
+
+        private void InitializeSearch()
+        {
+            searchBox = new TextBox
+            {
+                Dock = DockStyle.Top,
+                PlaceholderText = "Search tree view"
+            };
+            searchBox.TextChanged += (s, e) =>
+            {
+                var searchText = searchBox.Text.ToLower();
+                foreach (TreeNode node in treeView.Nodes)
+                {
+                    SearchNodes(node, searchText);
+                }
+            };
+            splitContainer.Panel1.Controls.Add(searchBox);
+        }
+
+        private void UpdateComboBoxItems(List<VerificationResponse> responses)
+        {
+            comboBoxResponses.Items.Clear();
+            for (int i = 0; i < responses.Count; i++)
+            {
+                comboBoxResponses.Items.Add($"Response {i + 1} - Batch: {responses[i].Batch.Name} ({responses[i].ExecutionDate})");
+            }
         }
 
         private void ComboBoxResponses_SelectedIndexChanged(object sender, EventArgs e)
         {
             if (comboBoxResponses.SelectedIndex >= 0)
             {
-                InitializeTreeView(_verificationResponses[comboBoxResponses.SelectedIndex]);
+                var verificationResponse = _verificationResponses[comboBoxResponses.SelectedIndex];
+                InitializeTreeView(verificationResponse);
+                InitializeDocumentsGrid(verificationResponse);
             }
         }
 
         private void InitializeTreeView(VerificationResponse verificationResponse)
         {
             treeView.Nodes.Clear();
-            //var rootNode = treeView.Nodes.Add($"VerificationResponse (ID: {verificationResponse.VerificationResponseId})");
-            var rootNode = treeView.Nodes.Add($"VerificationResponse (Date:  {verificationResponse.ExecutionDate})");
+            var rootNode = treeView.Nodes.Add($"VerificationResponse (Date: {verificationResponse.ExecutionDate})");
             rootNode.Tag = verificationResponse;
+            rootNode.ImageIndex = 0;
+            rootNode.SelectedImageIndex = 0;
 
             // Add basic verification response properties
             rootNode.Nodes.Add($"Status: {verificationResponse.Status}");
             rootNode.Nodes.Add($"Execution Date: {verificationResponse.ExecutionDate}");
             if (!string.IsNullOrEmpty(verificationResponse.ErrorMessage))
-                rootNode.Nodes.Add($"Error: {verificationResponse.ErrorMessage}");
+            {
+                var errorNode = rootNode.Nodes.Add($"Error: {verificationResponse.ErrorMessage}");
+                errorNode.ForeColor = Color.Red;
+            }
 
             // Add Batch information
             if (verificationResponse.Batch != null)
             {
                 var batchNode = rootNode.Nodes.Add($"Batch: {verificationResponse.Batch.Name}");
                 batchNode.Tag = verificationResponse.Batch;
+                batchNode.ImageIndex = 1;
+                batchNode.SelectedImageIndex = 1;
 
                 batchNode.Nodes.Add($"Creation Date: {verificationResponse.Batch.CreationDate}");
-                    batchNode.Nodes.Add($"Close Date: {verificationResponse.Batch.CloseDate}");
+                batchNode.Nodes.Add($"Close Date: {verificationResponse.Batch.CloseDate}");
 
                 // Batch Class
                 if (verificationResponse.Batch.BatchClass != null)
@@ -102,71 +162,41 @@ namespace VerificationResponseViewer
                         stateNode.Tag = state;
                     }
                 }
-
-                // Documents
-                if (verificationResponse.Batch.Documents?.Count > 0)
-                {
-                    var docsNode = batchNode.Nodes.Add("Documents");
-                    foreach (var doc in verificationResponse.Batch.Documents)
-                    {
-                        var docNode = docsNode.Nodes.Add($"Document: {doc.Name}");
-                        docNode.Tag = doc;
-
-                        if (doc.DocumentClass != null)
-                            docNode.Nodes.Add($"Document Class: {doc.DocumentClass.Name}");
-
-                        // Document Fields
-                        if (doc.DocumentFields?.Count > 0)
-                        {
-                            var fieldsNode = docNode.Nodes.Add("Document Fields");
-                            foreach (var field in doc.DocumentFields)
-                            {
-                                var fieldNode = fieldsNode.Nodes.Add($"{field}");
-                                fieldNode.Tag = field;
-                            }
-                        }
-
-                        // Signatures
-                        if (doc.Signatures?.Count > 0)
-                        {
-                            var sigNode = docNode.Nodes.Add("Signatures");
-                            foreach (var sig in doc.Signatures)
-                            {
-                                var sigNodeItem = sigNode.Nodes.Add("Signature");
-                                sigNodeItem.Tag = sig;
-                            }
-                        }
-
-                        // Pages
-                        if (doc.Pages?.Count > 0)
-                        {
-                            var pagesNode = docNode.Nodes.Add("Pages");
-                            foreach (var page in doc.Pages)
-                            {
-                                var pageNode = pagesNode.Nodes.Add($"Page: {page.FileName}");
-                                pageNode.Tag = page;
-
-                                if (page.Sections is string sections && !string.IsNullOrEmpty(sections))
-                                {
-                                    pageNode.Nodes.Add("Sections: JSON Data");
-                                }
-
-                                if (page.PageTypes?.Count > 0)
-                                {
-                                    var typesNode = pageNode.Nodes.Add("Page Types");
-                                    foreach (var type in page.PageTypes)
-                                    {
-                                        var typeNode = typesNode.Nodes.Add($"{type.Name} (Confidence: {type.Confidence})");
-                                        typeNode.Tag = type;
-                                    }
-                                }
-                            }
-                        }
-                    }
-                }
             }
 
             treeView.ExpandAll();
+        }
+
+        private void InitializeDocumentsGrid(VerificationResponse verificationResponse)
+        {
+            documentsGrid.Rows.Clear();
+            documentsGrid.Columns.Clear();
+
+            documentsGrid.Columns.Add("Name", "Document Name");
+            documentsGrid.Columns.Add("Class", "Document Class");
+            documentsGrid.Columns.Add("FieldCount", "Field Count");
+            documentsGrid.Columns.Add("SignatureCount", "Signature Count");
+            documentsGrid.Columns.Add("PageCount", "Page Count");
+
+            foreach (var doc in verificationResponse.Batch?.Documents ?? new List<VerificationDocument>())
+            {
+                documentsGrid.Rows.Add(
+                    doc.Name,
+                    doc.DocumentClass?.Name ?? "N/A",
+                    doc.DocumentFields?.Count ?? 0,
+                    doc.Signatures?.Count ?? 0,
+                    doc.Pages?.Count ?? 0
+                );
+            }
+        }
+
+        private void SearchNodes(TreeNode node, string searchText)
+        {
+            node.BackColor = node.Text.ToLower().Contains(searchText) ? Color.Yellow : Color.White;
+            foreach (TreeNode child in node.Nodes)
+            {
+                SearchNodes(child, searchText);
+            }
         }
 
         private void treeView_AfterSelect(object sender, TreeViewEventArgs e)
@@ -175,9 +205,23 @@ namespace VerificationResponseViewer
             propertyGrid.SelectedObject = _selectedObject;
         }
 
+        private void treeView_NodeMouseHover(object sender, TreeNodeMouseHoverEventArgs e)
+        {
+            //if (e.Node.Tag is VerificationResponse vr)
+            //{
+            //    e.Node.ToolTipText = $"ID: {vr.VerificationResponseId}\nStatus: {vr.Status}\nDate: {vr.ExecutionDate}";
+            //}
+            //else 
+            if (e.Node.Tag is Batch batch)
+            {
+                e.Node.ToolTipText = $"Name: {batch.Name}\nCreated: {batch.CreationDate}";
+            }
+        }
+
         private void propertyGrid_PropertyValueChanged(object s, PropertyValueChangedEventArgs e)
         {
             treeView.Refresh();
+            documentsGrid.Refresh();
         }
     }
 }
